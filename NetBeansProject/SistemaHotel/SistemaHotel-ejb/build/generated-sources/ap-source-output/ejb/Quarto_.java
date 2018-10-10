14a71a4a2796b4eb8f642e21828f56b1ad010eb3
joaovenancio <joaovitorvenancio99@outlook.com>
2018-10-10 15:35:47 -0300

    Alterado o front do Cadastrar Quarto

--- NetBeansProject/SistemaHotel/SistemaHotel-ejb/build/generated-sources/ap-source-output/ejb/Quarto_.java
+++ NetBeansProject/SistemaHotel/SistemaHotel-ejb/build/generated-sources/ap-source-output/ejb/Quarto_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-10T15:19:38")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-10T15:34:08")
 @StaticMetamodel(Quarto.class)
 public class Quarto_ { 
 
--- NetBeansProject/SistemaHotel/SistemaHotel-war/src/java/ejb/UsuariohotelMBean.java
+++ NetBeansProject/SistemaHotel/SistemaHotel-war/src/java/ejb/UsuariohotelMBean.java
@@ -106,4 +106,6 @@ public class UsuariohotelMBean {
         return "listagem";
     }
     
+    
+    
 }
