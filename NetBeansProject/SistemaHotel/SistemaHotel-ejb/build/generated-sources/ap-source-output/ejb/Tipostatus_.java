8315244adfb267ef82f4cdf3eff31d1a2df07c0a
joaovenancio <joaovitorvenancio99@outlook.com>
2018-10-10 15:51:17 -0300

    Alterando o solicitarServico

--- NetBeansProject/SistemaHotel/SistemaHotel-ejb/build/generated-sources/ap-source-output/ejb/Tipostatus_.java
+++ NetBeansProject/SistemaHotel/SistemaHotel-ejb/build/generated-sources/ap-source-output/ejb/Tipostatus_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-10T15:34:08")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-10T15:50:17")
 @StaticMetamodel(Tipostatus.class)
 public class Tipostatus_ { 
 
